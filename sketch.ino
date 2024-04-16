27abf9478c3add196f20d1136237b4a7392b4a3b
Luna <84203950+Wolvinny@users.noreply.github.com>
2024-04-16 19:53:05 +0200

    Create sketch.ino

---
+++ sketch.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+
+
+#include "./display.h"
+
+#define OLED_RESET 4
+
+void setup() {
+
+  currentButtonState = digitalRead(BUTTON_PIN);
+  sensors.begin();
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+
+  Wire.begin();
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+
+  defaultDisplay();
+
+}
+
+int timer = 0;
+
+void loop() {
+
+  lastButtonState = currentButtonState;      // save the last state
+  currentButtonState = digitalRead(BUTTON_PIN); // read new state
+
+  if (lastButtonState == HIGH && currentButtonState == LOW) {
+    // toggle state of LED
+    ledState = !ledState;
+    // control LED arccoding to the toggled state
+    digitalWrite(LED_PIN, ledState);
+    defaultDisplay();
+    delay(250); //debounce 25 is genoeg
+  }
+
+  timer++;
+
+  if (timer >= 1000) {
+    timer = 0;
+    defaultDisplay();
+    checkSensors();
+  }
+
+
+
+  delay(1);
+}
+
+
