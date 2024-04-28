58606526b293d57b32e58b928097f0b57fca0792
Luna <84203950+Wolvinny@users.noreply.github.com>
2024-04-28 21:18:53 +0200

    Update checksensor.h

--- checksensor.h
+++ checksensor.h
@@ -1,9 +1,9 @@
-#include "./button.h"
-#include "./conf.h"
-
+#include "./button.h";
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <AverageValue.h>
+#include "./config.h"
+
 
 
 int wrn = false;
@@ -12,74 +12,61 @@ int printed = false;
 int trig = false;
 
 float maxT = 0;
-float minT = 50;
+float minT = 50; 
 
 OneWire oneWire(TEMP_PORT);
 DallasTemperature sensors(&oneWire);
 
 const long MAX_VALUES_NUM = 40;
-// 40 measurements
+//40 measurements
 AverageValue<float> averageValue(MAX_VALUES_NUM);
 
-void checkSensors()
-{
+
+void checkSensors() {
   sensors.requestTemperatures();
 
   float curr;
 
-  if (USE_F)
-  {
+    if(USE_F) {
     curr = sensors.getTempFByIndex(0);
-  }
-  else
-  {
+  } else {
     curr = sensors.getTempCByIndex(0);
   }
 
   averageValue.push(curr);
 
-  if (curr > maxT)
-  {
+  if (curr > maxT) {
     maxT = curr;
   }
 
-  if (curr < minT)
-  {
+  if (curr < minT) {
     minT = curr;
   }
 
-  if (curr > TEMP_WARN)
-  {
-    if (curr > TEMP_OVHT)
-    {
+  if (curr > TEMP_WARN) {
+    if (curr > TEMP_OVHT) {
 
-      if (!trig)
-      {
-        ledState = HIGH;
-        digitalWrite(LED_PIN, HIGH);
-        trig = true; // avoid this turning the fan on again if you manually turn it off
-      }
+     if(!trig)  {
+      ledState = HIGH;
+       digitalWrite(FAN_PORT, HIGH);
+       trig = true; //avoid this turning the fan on again if you manually turn it off
+     }
       ovht = true;
-    }
-    else
-    {
+    } else {
       wrn = true;
     }
   }
 
-  if (ovht && curr < TEMP_WARN)
-  {
+  if (ovht && curr < TEMP_WARN) {
     wrn = false;
-    if (ovht)
-    {
+    if (ovht) {
       ledState = LOW;
-      digitalWrite(LED_PIN, LOW);
+      digitalWrite(FAN_PORT, LOW);
       ovht = false;
       trig = false;
     }
+  }else if(wrn && curr  < TEMP_WARN) {
+     wrn = false;
   }
-  else if (wrn && curr < TEMP_WARN)
-  {
-    wrn = false;
-  }
+
 }
